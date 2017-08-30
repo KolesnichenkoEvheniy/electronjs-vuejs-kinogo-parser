600656d44dbc89d0dc61e7feefcf8b81dee8b482
Evgeniy <evgeniy.web@ukr.net>
2017-08-30 23:45:58 +0300

    temporary removed auto-update section

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -24,65 +24,65 @@ new Vue({
   template: '<App/>'
 }).$mount('#app')
 
-setText('version', updater.version);
-setText('build', updater.buildId);
-attachUiHandlers();
-attachUpdaterHandlers();
+// setText('version', updater.version);
+// setText('build', updater.buildId);
+// attachUiHandlers();
+// attachUpdaterHandlers();
 
-function attachUiHandlers() {
-  const btnUpdate        = document.getElementById('btn-update');
-  const btnInstall       = document.getElementById('btn-install');
-  const chkAutomatically = document.getElementById('automatically');
+// function attachUiHandlers() {
+//   const btnUpdate        = document.getElementById('btn-update');
+//   const btnInstall       = document.getElementById('btn-install');
+//   const chkAutomatically = document.getElementById('automatically');
 
-  btnUpdate.addEventListener('click', () => {
-    updater.checkForUpdates();
-    document.body.classList.add('update-downloading');
-  });
+//   btnUpdate.addEventListener('click', () => {
+//     updater.checkForUpdates();
+//     document.body.classList.add('update-downloading');
+//   });
 
-  btnInstall.addEventListener('click', () => {
-    updater.downloadUpdate();
-  });
+//   btnInstall.addEventListener('click', () => {
+//     updater.downloadUpdate();
+//   });
 
-  chkAutomatically.addEventListener('change', function() {
-    updater.setOptions('autoDownload', this.checked);
-  });
-}
+//   chkAutomatically.addEventListener('change', function() {
+//     updater.setOptions('autoDownload', this.checked);
+//   });
+// }
 
-function attachUpdaterHandlers() {
-  updater.on('update-available', onUpdateAvailable);
-  updater.on('update-downloading', onUpdateDownloading);
-  updater.on('update-downloaded', onUpdateDownloaded);
-  updater.setOptions('logger', {
-    info(text) { log('info', text); },
-    warn(text) { log('warn', text); }
-  });
+// function attachUpdaterHandlers() {
+//   updater.on('update-available', onUpdateAvailable);
+//   updater.on('update-downloading', onUpdateDownloading);
+//   updater.on('update-downloaded', onUpdateDownloaded);
+//   updater.setOptions('logger', {
+//     info(text) { log('info', text); },
+//     warn(text) { log('warn', text); }
+//   });
 
-  function onUpdateAvailable(meta) {
-    setText('new-version', meta.version);
-    setText('description', meta.readme);
-    document.body.className = 'update-available';
-  }
+//   function onUpdateAvailable(meta) {
+//     setText('new-version', meta.version);
+//     setText('description', meta.readme);
+//     document.body.className = 'update-available';
+//   }
 
-  function onUpdateDownloading() {
-    document.body.classList.add('update-downloading');
-  }
+//   function onUpdateDownloading() {
+//     document.body.classList.add('update-downloading');
+//   }
 
-  function onUpdateDownloaded() {
-    if (window.confirm('The app has been updated. Do you like to restart it now?')) {
-      updater.quitAndInstall();
-    }
-  }
+//   function onUpdateDownloaded() {
+//     if (window.confirm('The app has been updated. Do you like to restart it now?')) {
+//       updater.quitAndInstall();
+//     }
+//   }
 
-  function log(level, text) {
-    const logMessages = document.getElementById('log-messages');
-    const p = document.createElement('p');
-    p.appendChild(document.createTextNode(`[${level}] ${text}`));
-    logMessages.appendChild(p);
-  }
-}
+//   function log(level, text) {
+//     const logMessages = document.getElementById('log-messages');
+//     const p = document.createElement('p');
+//     p.appendChild(document.createTextNode(`[${level}] ${text}`));
+//     logMessages.appendChild(p);
+//   }
+// }
 
-function setText(id, text) {
-  document.getElementById(id).appendChild(
-    document.createTextNode(text)
-  );
-}
+// function setText(id, text) {
+//   document.getElementById(id).appendChild(
+//     document.createTextNode(text)
+//   );
+// }
