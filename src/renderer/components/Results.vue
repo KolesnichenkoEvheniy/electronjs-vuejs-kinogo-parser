997e1660c877469233b3c112e4316357d4cc3565
spasea <ithespv@gmail.com>
2017-09-04 23:49:24 +0300

    fix results

--- src/renderer/components/Results.vue
+++ src/renderer/components/Results.vue
@@ -1,10 +1,10 @@
 <template>
 	<el-row>
-		<el-col :span="6" v-for="(o, index) in results" :key="o" :offset="(index > 0 && index % 3 != 0) ? 2 : 0">
+		<el-col :span="6" v-for="(o, index) in results" :key="index" :offset="(index > 0 && index % 3 != 0) ? 2 : 0">
 			<el-card :body-style="{ padding: '0px' }">
 				<img :src="`${url}${o.image}`" class="image" @click="open(o.href)">
 				<div style="padding: 14px;">
-					<span v-text="o.title"></span>
+					<span v-text="o.title" class="text-span"></span>
 					<div class="bottom clearfix">
 						<el-button type="text" class="button" @click="open(o.href)">Watch film</el-button>
 					</div>
@@ -30,6 +30,16 @@
 </script>
 
 <style scoped>
+	.el-col-1, .el-col-10, .el-col-11, .el-col-12, .el-col-13, .el-col-14, .el-col-15, .el-col-16, .el-col-17, .el-col-18, .el-col-19, .el-col-2, .el-col-20, .el-col-21, .el-col-22, .el-col-23, .el-col-24, .el-col-3, .el-col-4, .el-col-5, .el-col-6, .el-col-7, .el-col-8, .el-col-9 {
+		float: none;
+		margin-bottom: 66px;
+	}
+
+	.el-row {
+		display: flex;
+		flex-flow: row wrap;
+	}
+
 	.time {
 		font-size: 13px;
 		color: #999;
